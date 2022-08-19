c5f63e9fb9dbe6f1a94d1c68500b0efbcc0ea86f
anacondz <97612929+anacondz@users.noreply.github.com>
2022-08-19 21:30:30 +0300

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingrid = document.querySelector("#ingredients");
+const items = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.textContent = ingredient;
+  return item;
+});
+
+ingrid.append(...items);
+console.log(ingrid)
+console.log(items)
