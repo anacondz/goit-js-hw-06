6284e32a3aa2ccf7e7895d6334a460c5ab35126b
anacondz <97612929+anacondz@users.noreply.github.com>
2022-08-22 16:40:24 +0300

    9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const color = document.querySelector('.color');
+const buttonCh = document.querySelector('.change-color');
+
+buttonCh.addEventListener("click", () => {
+  color.textContent = getRandomHexColor();
+  document.body.setAttribute(
+    'style', 'background-color:' + color.textContent
+  )
+});
