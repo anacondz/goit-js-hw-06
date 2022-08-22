42bf45b7d5a4eb0400ce90ec4bb90c5fafa8be96
anacondz <97612929+anacondz@users.noreply.github.com>
2022-08-22 16:11:55 +0300

    task 8

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -2,6 +2,7 @@ const range = document.getElementById('font-size-control');
 const abra = document.getElementById('text');
 
 range.addEventListener('range', change);
+
 function change(event) {
     abra.style.fontSize = event.currentTarget.value + 'px';
 }
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const form1 = document.querySelector('.login-form');
 
+form1.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    const formEl = event.currentTarget.elements;
+
+    const email = formEl.email;
+    const password = formEl.password;
+
+    if (email.value === "" || password.value === "") {
+        return alert('Форма не полная!');
+    }
+
+    console.log({email: email.value , password: password.value});
+}
+form1.reset();
